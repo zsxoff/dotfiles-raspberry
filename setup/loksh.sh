0a191cb937374644fd4c59f0bb9047286b3fff45
zsxoff <zsxoff@gmail.com>
2019-03-25 22:56:10 +0400

    add loksh setup

---
+++ setup/loksh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /tmp
+git clone https://github.com/dimkr/loksh
+cd ./loksh
+make; sudo make install
+
+echo "/usr/bin/ksh" | sudo tee --append /etc/shells
+chsh -s /usr/bin/ksh
+sudo chsh -s /usr/bin/ksh
