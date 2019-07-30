d8b625dcd86f83f48e74cf1d7488ef5b4fadecfd
zsxoff <zsxoff@gmail.com>
2019-07-30 07:07:32 +0400

    merge systools and utils

--- setup/systools.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S --needed --noconfirm \
-acpid \
-dialog \
-fuse \
-highlight \
-psmisc \
-usbutils
--- setup/utils.sh
+++ setup/utils.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 sudo pacman -S --needed --noconfirm \
+acpid \
 aria2 \
 curl \
+dialog \
+fuse \
+highlight \
 nano \
+psmisc \
 tmux \
 unrar \
 unzip \
+usbutils \
 vim \
 wget
